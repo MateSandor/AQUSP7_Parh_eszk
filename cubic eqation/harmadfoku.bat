81c2575a45a17fa4f2cb7dda93c2d6330c738f59
MateSandor <79148317+MateSandor@users.noreply.github.com>
2023-05-01 23:08:53 +0200

    Delete cubic eqation directory

--- cubic eqation/harmadfoku.bat
+++
@@ -1,3 +0,0 @@
-SET PATH=%PATH%;%cd%\MinGW\bin
-gcc harmadfoku.c -o harmadfoku.exe&& harmadfoku.exe
-cmd
--- cubic eqation/harmadfoku_gpu.bat
+++
@@ -1,3 +0,0 @@
-SET PATH=%PATH%;%cd%\MinGW\bin
-gcc harmadfoku_gpu.c src/kernel_loader.c -o harmadfoku_gpu.exe -lOpenCL -Iinclude && harmadfoku_gpu.exe
-cmd
