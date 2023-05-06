70f7856849b4b368cefa87a06427ce1cca22fec6
MateSandor <samate113@gmail.com>
2023-05-07 00:28:13 +0200

    Revert "Patched some mistakes extended description below"
    This reverts commit 3463f242c23d8813ec6ce9c93224599617b7785d.

--- cubic_equation.bat
+++
@@ -1,3 +0,0 @@
-SET PATH=%PATH%;%cd%\MinGW\bin
-gcc cubic_equation.c -o cubic_equation.exe&& cubic_equation.exe
-cmd
--- cubic_equation_gpu.bat
+++
@@ -1,3 +0,0 @@
-SET PATH=%PATH%;%cd%\MinGW\bin
-gcc cubic_equation_gpu.c src/kernel_loader.c -o cubic_equation_gpu.exe -lOpenCL -Iinclude && cubic_equation_gpu.exe
-cmd
